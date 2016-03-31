55a59f1007a9d7228e32ca05589e2a738b874b01
Ranjit <ranjitiyer@gmail.com>
2016-03-31 13:27:42 -0700

    Create cheatsheet.py

---
+++ src/cheatsheet.py
@@ -0,0 +1,47 @@
+def common_imports:
+
+def parse_args:
+
+def listfiles:
+
+def mkdirs:
+
+def chdir:
+
+def os_apis:
+
+def parse_json:
+
+def parse_xml:
+
+def http_client:
+
+def zip_unzip:
+
+def env_vars:
+
+def control_structures:
+
+def exception_handling:
+
+def writefiles:
+
+def readfiles:
+
+def read_line_by_line:
+
+def write_line_by_line:
+
+def string_formatting:
+
+def string_buffer:
+
+def string_manipulation:
+
+def list_operations:
+
+def map_operations:
+
+
+
+
